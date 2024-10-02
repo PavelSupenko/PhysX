add1b7231d77f30752ee6cb93f93ea143a2c8b5e
Pavlo Supenko <pavlo.supenko@bini.games>
2024-10-02 15:54:50 +0300

    Add ability for MacOS on arm use SIMD vectors instructions

--- blast/source/shared/NsFoundation/include/NsVecMath.h
+++ blast/source/shared/NsFoundation/include/NsVecMath.h
@@ -51,7 +51,7 @@
 //TODO: dima: reference all platforms with SIMD support here,
 //all unknown/experimental cases should better default to NO SIMD.
 
-#if NV_X86 || NV_X64 || NV_WINRT || NV_PS3 || NV_X360 || (NV_LINUX && (NV_X86 || NV_X64)) || (NV_ANDROID && NV_NEON) || NV_XBOXONE
+#if NV_X86 || NV_X64 || NV_WINRT || NV_PS3 || NV_X360 || (NV_LINUX && (NV_X86 || NV_X64)) || (NV_ANDROID && NV_NEON) || NV_XBOXONE || (NV_APPLE_FAMILY && NV_NEON)
 #define COMPILE_VECTOR_INTRINSICS 1 // use SIMD         
 #else
 #define COMPILE_VECTOR_INTRINSICS 0 // do not use SIMD
